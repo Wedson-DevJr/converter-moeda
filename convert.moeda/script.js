25487d7fd5e2643683c7963110324b91e4696e5a
Wedson-DevJr <149250232+Wedson-DevJr@users.noreply.github.com>
2024-03-04 23:42:51 -0300

    achei fassiuZim

--- convert.moeda/script.js
+++ convert.moeda/script.js
@@ -1,50 +1,32 @@
-let quantia = parseFloat(document.querySelector('#quantia').value)
-let opcion01 = document.querySelector('#de')
-let opcion02 = document.querySelector('#para')
+
 
 let res = document.querySelector('.resul-p')
 
 function clicou(){
+    let quantia = parseFloat(document.querySelector('#quantia').value)
+    let Mmoeda1 = document.querySelector('#de').value
+    let Mmoeda2 = document.querySelector('#para').value
+
     if(quantia.length == 0 || Number(quantia) < 0){
         alert('valor invalido')
     } else {
-        let real = 1
-        let dolar = 4.95
-        let euro = 5.37
-        let btic = 331826.12
-        let valor = Number(quantia)
+        /* pegar a  quantia
+        pegar a moeda 1 e dividir pela moeda 2*/
 
-        let n = ''
-        let n1 = quantia * n
+        function coverteValor(moeda1, moeda2) {
+            fetch(`https://economia.awesomeapi.com.br/json/last/${moeda1}-${moeda2}`)
+            .then((r)=>r.json())
+            .then((dado)=>{
+            const cotacao = dado[`${moeda1}${moeda2}`];
+            if (cotacao) {
+                let _bid = Number(cotacao.bid)
+                console.log(`Bid: ${_bid}`);
 
-        switch(opcion01.value){
-            case '0':
-
-                break
-            case '1':
-
-                break
-            case '2':
-
-                break
-            case '3':
-
-                break
+                let valor = quantia * _bid
+                res.innerHTML = `${valor}`
+            }
+            })
         }
-
-        switch(opcion02.value){
-            case '4':
-
-                break
-            case '5':
-
-                break
-            case '6':
- 
-                break
-            case '7':
-
-                break
-        }
+        coverteValor(`${Mmoeda1}`, `${Mmoeda2}`);
     }
 }
